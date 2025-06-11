2a1761df9f35229b1fff951e7ae94e392a908f37
cmku408 <47934310+cmku408@users.noreply.github.com>
2025-06-10 17:09:56 -0700

    moving folders
    cleaning up structure

--- MATLAB/Project/fcn_trajectory.m
+++
@@ -1,8 +0,0 @@
-function [qd qd_dot qd_dotdot] = fcn_trajectory(t)
-
-%Desired trajectory
-qd = ... %Desired joint angles at time t
-qd_dot = ... %Desired joint velocities at time t
-qd_dotdot = ... %Desired joint accelerations at time t
-
-end
